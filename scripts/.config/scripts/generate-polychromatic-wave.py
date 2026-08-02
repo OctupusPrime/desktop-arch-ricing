ac37f1e6cd9514ea94d576a125db3276178a3763
OctupusPrime <m.sokil.dev@gmail.com>
2026-08-03 01:17:28 +0200

    Add Polychromatic Wave effect generator

---
+++ scripts/.config/scripts/generate-polychromatic-wave.py
@@ -0,0 +1,49 @@
+import os
+import json
+
+# Animation parameters
+colors = [
+    "#142015", "#103814", "#056a1e", "#086f19", "#249134", "#40a943",
+    "#7ed787", "#b7efc5", "#80d983", "#7ed787", "#40a943", "#249134",
+    "#086f19", "#056a1e", "#103814", "#142015"
+]
+map_cols = 18
+map_rows = 6
+num_frames = len(colors)
+
+frames = []
+for frame_idx in range(num_frames):
+    frame = {}
+    for col in range(map_cols):
+        frame[str(col)] = {}
+        for row in range(map_rows):
+            # Reverse direction: bottom right to top left
+            color_idx = (int((map_cols - 1 - col) + 1.5 * (map_rows - 1 - row)) + frame_idx) % len(colors)
+            frame[str(col)][str(row)] = colors[color_idx]
+    frames.append(frame)
+
+output = {
+    "name": "Wave",
+    "type": 3,
+    "author": "OctupusPrime",
+    "icon": "img/options/wave.svg",
+    "summary": "",
+    "map_device": "Razer Huntsman Tournament Edition",
+    "map_device_icon": "keyboard",
+    "map_graphic": "huntsman_te_en_US.svg",
+    "map_cols": map_cols,
+    "map_rows": map_rows,
+    "save_format": 8,
+    "revision": 1,
+    "fps": 15,
+    "loop": True,
+    "frames": frames
+}
+
+effect_path = os.path.expanduser("~/.config/polychromatic/effects/Wave.json")
+os.makedirs(os.path.dirname(effect_path), exist_ok=True)
+
+with open(effect_path, "w") as f:
+    json.dump(output, f, indent=2)
+
+print("Wave.json was generated successfully at:", effect_path)
